5116e306facd01dbf987945ddfd19eca5efd1b32
Mark Skilbeck <mark.skilbeck@rigetti.com>
2020-05-01 22:22:48 -0700

    good letter | bad letter
    improve handling of keys

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 
 let poem = "The moving finger writes; and, having writ, moves on.";
 
+let goodLetterStyle = "active-letter-good",
+    badLetterStyle  = "active-letter-bad";
+
 class Letter extends React.Component {
   render() {
     let i = this.props.i,
         c = this.props.c,
-        cursor = this.props.cursor;
+        cursor = this.props.cursor,
+        cursorStyle = this.props.cursorStyle;
 
     var letterid    = "letter-" + i,
-        letterclass = i == cursor ? "active-letter-good" : "inactive-letter";
+        letterclass = i == cursor ? cursorStyle : "inactive-letter";
 
     return <span id={letterid} key={letterid} className={letterclass}>
              {c}
@@ -23,7 +27,9 @@ class Phrase extends React.Component {
   spanify() {
     return (
       <span>
-        {this.props.str.split("").map((c, i) => <Letter key={i} i={i} c={c} cursor={this.props.cursor} />)}
+        {this.props.str.split("")
+         .map((c, i) => <Letter key={i} i={i} c={c} cursor={this.props.cursor}
+                               cursorStyle={this.props.cursorStyle}/>)}
       </span>
     );
   }
@@ -41,6 +47,7 @@ class Main extends React.Component {
 
     this.state = {
       cursor: 0,
+      cursorStyle: goodLetterStyle,
       str: poem
     };
 
@@ -52,9 +59,9 @@ class Main extends React.Component {
   }
 
   handleKey(e) {
-    // TODO This disables stuff like cmd+r. Not good.
-    // Printing keys and backspace.
-    if ((e.keyCode >= 32 && e.keyCode <= 223) || e.keyCode == 8) {
+    console.log(e);
+    if ((e.keyCode >= 32 && e.keyCode <= 223 && !e.altKey && !e.ctrlKey && !e.metaKey)
+        || e.keyCode == 8) {
       // Prevent keys like backspace affecting the page. Controversial,
       // but the app is keypress-based and so not unreasonable that it
       // would override some keys. Maybe this should be relaxed: right
@@ -66,14 +73,26 @@ class Main extends React.Component {
       return;
     }
 
-    this.setState({cursor: (this.state.cursor + 1) % this.state.str.length});
+    var newstate;
+    console.log(this.state.cursor, this.state.str[this.state.cursor], e.keyCode);
+    
+    if (this.state.str[this.state.cursor] == e.key) {
+      newstate = {cursor: (this.state.cursor + 1) % this.state.str.length,
+                  cursorStyle: goodLetterStyle};
+    }
+    else {
+      newstate = {cursor: this.state.cursor, cursorStyle: badLetterStyle};  
+    }
+    this.setState(newstate);
   }
 
   render() {
     return (
       <div className="main">
         <div className="poem">
-          <Phrase cursor={this.state.cursor} str={this.state.str} />
+          <Phrase str={this.state.str}
+                  cursor={this.state.cursor}
+                  cursorStyle={this.state.cursorStyle} />
         </div>
       </div>
     );

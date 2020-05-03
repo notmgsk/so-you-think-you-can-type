26bdcd7cd5d607b23de76887a0a44db68d0147ef
Mark Skilbeck <mark.skilbeck@rigetti.com>
2020-05-03 15:09:04 -0700

    add more quores

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//let poem = "The moving finger writes; and, having writ, moves on.";
-let poem = "The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day vindicate the vigilant and the virtuous. Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it's my very good honor to meet you and you may call me V.";
-
+let quotes = [
+  // Einstein
+  "A human being is a part of the whole called by us universe, a part limited in time and space. He experiences himself, his thoughts and feeling as something separated from the rest, a kind of optical delusion of his consciousness. This delusion is a kind of prison for us, restricting us to our personal desires and to affection for a few persons nearest to us. Our task must be to free ourselves from this prison by widening our circle of compassion to embrace all living creatures and the whole of nature in its beauty.",
+  // Feynman
+  "Well, Mr. Frankel, who started this program, began to suffer from the computer disease that anybody who works with computers now knows about. It's a very serious disease and it interferes completely with the work. The trouble with computers is you *play* with them. They are so wonderful. You have these switches - if it's an even number you do this, if it's an odd number you do that - and pretty soon you can do more and more elaborate things if you are clever enough, on one machine.",
+  // What's his face who wrote V for Vendetta
+  "The only verdict is vengeance; a vendetta, held as a votive, not in vain, for the value and veracity of such shall one day vindicate the vigilant and the virtuous. Verily, this vichyssoise of verbiage veers most verbose, so let me simply add that it's my very good honor to meet you and you may call me V.",
+  // What's his face who wrote the quote.
+  "The moving finger writes; and, having writ, moves on.",
+  "A breeze ruffled the neat hedges of Privet Drive, which lay silent and tidy under the inky sky, the very last place you would expect astonishing things to happen. Harry Potter rolled over inside his blankets without waking up. One small hand closed on the letter beside him and he slept on, not knowing he was special, not knowing he was famous, not knowing he would be woken in a few hours' time by Mrs. Dursley's scream as she opened the front door to put out the milk bottles, nor that he would spend the next few weeks being prodded and pinched by his cousin Dudley...He couldn't know that at this very moment, people meeting in secret all over the country were holding up their glasses and saying in hushed voices: \"To Harry Potter - the boy who lived!\""
+];
 
 let goodLetterStyle = "active-letter-good",
     badLetterStyle  = "active-letter-bad";
@@ -45,12 +53,12 @@ class Phrase extends React.Component {
 
 class WPM extends React.Component {
   countWords(str) {
-    return poem.split(" ").length;
+    return str.split(" ").length;
   }
 
   render() {
     let wpm =
-        Number.parseFloat(this.countWords(this.props.str)/(this.props.clock / 60) * 1000)
+        Number.parseFloat(this.countWords(this.props.string)/(this.props.clock / 60) * 1000)
         .toFixed();
     return (
       <h1>WPM {wpm}</h1>
@@ -65,7 +73,7 @@ class Main extends React.Component {
     this.state = {
       cursor: 0,
       cursorStyle: goodLetterStyle,
-      str: poem
+      str: this.randomElt(quotes)
     };
 
     this.clock = null;
@@ -119,10 +127,20 @@ class Main extends React.Component {
     this.setState(newstate);
   }
 
+  randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  randomElt(array) {
+    return array[this.randomInt(0, array.length)];
+  }
+
   render() {
     return (
       <div className="main">
-        <div className="poem">
+        <div className="quote">
           <Phrase str={this.state.str}
                   cursor={this.state.cursor}
                   cursorStyle={this.state.cursorStyle} />
